684fc1038d7418ba91b00210dd5afb842b9b559f
TimurDuyshenaliev <timaster999@gmail.com>
2022-10-01 14:23:09 +0300

    hw9 first task done

---
+++ hw9(pro)/task1.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn');
+let color = function (event){
+    const block = document.getElementById('text');
+    
+    if(text.style.color){
+        text.style.color = '';
+    }
+    else text.style.color = 'blue';
+}
+btn.addEventListener('click', color)    
