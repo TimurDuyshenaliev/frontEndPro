83b79188fc048578ac35960889ba16b039b511f8
TimurDuyshenaliev <timaster999@gmail.com>
2022-10-08 14:25:51 +0300

    task 1 upd

--- hw(10)pro/task1.js
+++ hw(10)pro/task1.js
@@ -1,9 +1,9 @@
 const link = document.getElementById('btn-link');
 const visit = document.getElementById('btn-visit');
-let getLink = function(event){
-    site = prompt('Напиши ссылку для перехода');
-    let linkTo = function(event){
-        location = site;
+let getLink = function(){
+    let site = prompt('Напиши ссылку для перехода');
+    let linkTo = function(){
+        window.location = site;
     }
     visit.addEventListener('click', linkTo);
 }
