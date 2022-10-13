200461a1b816b481d69142784bdcfb837b43ff56
TimurDuyshenaliev <timaster999@gmail.com>
2022-10-13 23:57:14 +0300

    hw12

--- hw12(pro)/script.js
+++ hw12(pro)/script.js
@@ -1,39 +1,55 @@
 document.addEventListener('DOMContentLoaded',function(){
     function Slider(prev, next, list){
-        this._prev = document.querySelector(prev);
-        this._next = document.querySelector(next);
-        this._list = document.querySelector(list);
-        this.length = this._list.getElementsByTagName('li').length;
+        const _prev = document.querySelector(prev);
+        const _next = document.querySelector(next);
+        const _list = document.querySelector(list);
+        this.length = _list.getElementsByTagName('li').length;
         this.changeItem = function ({ active, _newElement }) {
             if (_newElement) {
                 active.classList.remove('active');
                 _newElement.classList.add('active');
             }
-            else if(this.length == 1){
-                this._prev.classList.add('slider__prev-disable');
-                
-            }
-            else if(this.length -1){
-                this._next.classList.add('slider__next-disable');            
-            }
         };
+        
+        function disableButton(){
+            let activeItem = 1;
+            _prev.addEventListener('click', function(){
+                activeItem--;
+                if(activeItem == 1){
+                    _prev.classList.add('.disable')
+                }
+            })
+            _next.addEventListener('click', function(){
+                activeItem++;
+                if(activeItem == this.length -1){
+                    _prev.classList.add('.disable')
+                }
+            })
+            console.log(activeItem);
+        }
+        disableButton();
+        
+
+
+
         return console.log(this);
+
     }
     const slider = new Slider('.js--slider__prev','.js--slider__next','.js--list');
 
-    slider._next.addEventListener('click', function () {
-        const activeItem = slider._list.querySelector('.active');
+    document.querySelector('.js--slider__next').addEventListener('click', function () {
+        const activeItem = document.querySelector('.js--list').querySelector('.active');
         slider.changeItem({
             _newElement: activeItem.nextElementSibling,
             active: activeItem,
         });
     });
 
-    slider._prev.addEventListener('click', function () {
-        const activeItem = slider._list.querySelector('.active');
+    document.querySelector('.js--slider__prev').addEventListener('click', function () {
+        const activeItem = document.querySelector('.js--list').querySelector('.active');
         slider.changeItem({
             active: activeItem,
             _newElement: activeItem.previousElementSibling
         });
     })
-})
+})  
