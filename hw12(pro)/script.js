c2eb7e58ce67cc9c274beb99046b6873e1d4572d
TimurDuyshenaliev <timaster999@gmail.com>
2022-10-15 01:50:52 +0300

    upd hw 15.10

--- hw12(pro)/script.js
+++ hw12(pro)/script.js
@@ -5,52 +5,52 @@ document.addEventListener('DOMContentLoaded',function(){
         const _list = document.querySelector(list);
         this.length = _list.getElementsByTagName('li').length;
         this.changeItem = function ({ active, _newElement }) {
-            
             if (_newElement) {
                 active.classList.remove('active');
                 _newElement.classList.add('active');
             }
-            
         };
 
-        
+        _next.addEventListener('click', function () {
+            const activeItem = _list.querySelector('.active');
+            slider.changeItem({
+                _newElement: activeItem.nextElementSibling,
+                active: activeItem,
+            });
+        });
+    
+        _prev.addEventListener('click', function () {
+            const activeItem = _list.querySelector('.active');
+            slider.changeItem({
+                active: activeItem,
+                _newElement: activeItem.previousElementSibling
+            });
+        });
+
         let disableButton = function(){
             let activeSlide = 1;
+            _prev.classList.add('disable');
             _prev.addEventListener('click', function(){
-                activeSlide--;
+                if(activeSlide > 1){
+                    activeSlide--;
+                }
                 _next.classList.remove('disable');
-                if(activeSlide == 1){
+                if(activeSlide === 1){
                     _prev.classList.add('disable')
                 }
             });
             _next.addEventListener('click', function(){ 
-                activeSlide++;
+                if(activeSlide !== slider.length){
+                    activeSlide++;
+                }
                 _prev.classList.remove('disable');
-                if(activeSlide == _list.getElementsByTagName('li').length){
+                if(activeSlide === slider.length){
                     _next.classList.add('disable');
                 }
             });
         }
-        disableButton();    
-        
-        return console.log(this);
-
+        disableButton();
     }
     const slider = new Slider('.js--slider__prev','.js--slider__next','.js--list');
-    document.querySelector('.js--slider__next').addEventListener('click', function () {
-        const activeItem = document.querySelector('.js--list').querySelector('.active');
-        slider.changeItem({
-            _newElement: activeItem.nextElementSibling,
-            active: activeItem,
-        });
-    });
-
-    document.querySelector('.js--slider__prev').addEventListener('click', function () {
-        const activeItem = document.querySelector('.js--list').querySelector('.active');
-        slider.changeItem({
-            active: activeItem,
-            _newElement: activeItem.previousElementSibling
-        });
-    })
-    
-})  
+    console.log(slider);    
+});
