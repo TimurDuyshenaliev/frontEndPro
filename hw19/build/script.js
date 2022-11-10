2a30e5849405febf5c31eb685bb37ede729b6e18
TimurDuyshenaliev <timaster999@gmail.com>
2022-11-10 02:29:04 +0200

    all works

--- hw19/build/script.js
+++ hw19/build/script.js
@@ -1,11 +1,29 @@
-const data = [undefined, 2, 3, 4, 5, 'sd'];
-function testData([arg1, arg2, arg3, ...rest]) {
+"use strict";
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toArray(arr) { return _arrayWithHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+var data = [undefined, 2, 3, 4, 5, 'sd'];
+function testData(_ref) {
+  var _ref2 = _toArray(_ref),
+    arg1 = _ref2[0],
+    arg2 = _ref2[1],
+    arg3 = _ref2[2],
+    rest = _ref2.slice(3);
   if (data.length < 5) {
     console.log("Error");
   }
   if (arg1 === undefined) {
     arg1 = "Error";
   }
-  console.log(`${arg1} - ${arg2} - ${arg3}`);
-};
+  console.log("".concat(arg1, " - ").concat(arg2, " - ").concat(arg3));
+  console.log(_objectSpread({}, rest));
+}
+;
 testData(data);
