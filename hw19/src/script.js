307279d4ebb7de886ea191b85c55a1616b04140f
TimurDuyshenaliev <timaster999@gmail.com>
2022-11-10 02:21:36 +0200

    task done but babel not work

---
+++ hw19/src/script.js
@@ -0,0 +1,12 @@
+const data = [undefined,2,3,4,5,'sd'];
+function testData([arg1,arg2,arg3,...rest]){
+   if(data.length < 5){
+    console.log("Error");
+   }
+   if(arg1 === undefined){
+    arg1 = "Error";
+   }
+   console.log(`${arg1} - ${arg2} - ${arg3}`);
+   console.log({...rest});
+};
+testData(data);
