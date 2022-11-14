850748d49378d38bd870a126fb9292d20745da6d
TimurDuyshenaliev <timaster999@gmail.com>
2022-11-14 22:42:42 +0200

    fix bugs

--- hw20/script.js
+++ hw20/script.js
@@ -1,22 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(){
     let input = document.querySelector(".input");
     
-    document.querySelector(".submit").addEventListener("click",function(){
+    document.querySelector(".submit").addEventListener("click",function(event){
+        event.preventDefault()
         let value = input.value;
         input.value = "";
-        fetch(`https://jsonplaceholder.typicode.com/posts/${value}`)
-        .then((response) => response.json())
-        .then((json) => document.querySelector(".post").insertAdjacentHTML("afterbegin",json.body))
-        .then(document.querySelector(".btn-comm").addEventListener("click",function(){
-            fetch(`https://jsonplaceholder.typicode.com/posts/${value}/comments`)
+        if(value > 0 && value <= 10){
+            fetch(`https://jsonplaceholder.typicode.com/posts/${value}`)
             .then((response) => response.json())
-            .then((json) => {
-                for( let i = 0; i < 5; i++){
-                    document.querySelector(".post_comment").insertAdjacentHTML("afterbegin", json[i].body );
-                }
-                
-            })
-        }))
+            .then((json) => document.querySelector(".post").insertAdjacentHTML("afterbegin",`<br>`+ json.body + `<br>`))
+            .then(document.querySelector(".btn-comm").addEventListener("click",function(){
+                fetch(`https://jsonplaceholder.typicode.com/posts/${value}/comments`)
+                .then((response) => response.json())
+                .then((json) => {
+                    for( let i = 0; i < 5; i++){
+                        document.querySelector(".post_comment").insertAdjacentHTML("afterbegin",`<br>`+ json[i].body + `<br>`);
+                    }
+                    
+                })
+            }))
+        }
+        
     });
     
 })
