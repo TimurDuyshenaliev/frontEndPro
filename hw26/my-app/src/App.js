6967aaad4526cf8c0b8fe67d68d0acfcbcf421c5
TimurDuyshenaliev <timaster999@gmail.com>
2022-12-24 22:46:38 +0200

    hw done

---
+++ hw26/my-app/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <p>Timur</p>
+      <p>Duishenaliev</p>
+      <p>Stack: HTML, CSS, JS, GIT, Webpack</p>
+    </div>
+  );
+}
+
+export default App;
