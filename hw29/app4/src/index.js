6ebd6e3e8a33b5415339e1c1d0223879df350b87
TimurDuyshenaliev <timaster999@gmail.com>
2023-01-10 00:22:15 +0200

    fix

--- hw29/app4/src/index.js
+++ hw29/app4/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import { Header } from './сomponents/header';
-import {Main} from './pages/Main';
 import {Contact} from './pages/Contact';
 import { ErrorBoundary } from './сomponents/ErrorBoundary';
 
@@ -11,14 +10,17 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 function CreateNewStack(state) {
   const {value} = state;
   return(
-    <div>{value}</div>
+    <span>Stack:{value}</span>
   )
 }
 class StackForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      value: '',
+      stack: ''
+    };
+    this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -29,42 +31,40 @@ class StackForm extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     this.setState({
-      value: this.state.value + ' ' + event.target[0].value
+      stack: this.state.stack + ' ' + event.target[0].value
       },() => event.target[0].value = '');
-    console.log(this.state.value.length);
-    console.log(event.target[0].value)
   }
   
-  render() {
-    const {value} = this.state;
-    console.log(value)
-    return (
-      <>
+  render(){
+    const {value, stack} = this.state;
+    return(
       <form onSubmit={this.handleSubmit}>
-          <label>
-            Stack:
-            <input type="text"  onChange={this.handleChange} />
-          </label>
-          <button type="submit" disabled= { this.state.value.length < 5}> Add</button>
-        </form></>
+        <CreateNewStack value ={stack}/>
+        <br/>
+        <label>
+          New Stack:
+          <input type="text" value={value} onChange={this.handleChange} />
+        </label>
+        <button type="submit" disabled= {value.length < 5}> Add</button>
+      </form>
     );
   }
 }
 
 root.render(
   <ErrorBoundary>
-     <BrowserRouter>
-                    <Header/>
-                    <><div className="App">
-                      <p>Timur</p>
-                      <p>Duishenaliev</p>
-                      <p>Stack:</p>
-                      <CreateNewStack />
-                    </div></>
-                    <Routes>
-                        <Route path="/" element={<StackForm/>}/>
-                        <Route path="/contact" element={<Contact/>}/>
-                    </Routes>
-                </BrowserRouter>
+    <BrowserRouter>
+      <Header/>
+      <>
+        <div className="App">
+          <p>Timur</p>
+          <p>Duishenaliev</p>
+        </div>
+      </>
+      <Routes>
+        <Route path="/" element={<StackForm/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
   </ErrorBoundary>
 )
